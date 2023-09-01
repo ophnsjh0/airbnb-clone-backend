94d4a6101e2db64ee709c2473e052fc25a9154ab
shinjunghun <61132132+ophnsjh0@users.noreply.github.com>
2023-09-01 17:31:28 +0900

    commit

--- houses/models.py
+++ houses/models.py
@@ -2,14 +2,19 @@ from django.db import models
 
 # Create your models here.
 
-
 class House(models.Model):
 
     """ Model Definition for Houses """
 
     name = models.CharField(max_length=140)
-    price = models.PositiveIntegerField()
+    price_per_night = models.PositiveIntegerField()
     description = models.TextField()
     address = models.CharField(max_length=140)
+    pets_allowed = models.BooleanField(default=True)
+    
+    """ models.Model 클래스로 부터 상속받은 houses의 object name을 self,name으로 오버라이딩 """
+    def __str__(self):
+        return self.name
+
 
     
