f986a8bdca7bd301a999cb38215e818734b995d0
ophnsjh0 <61132132+ophnsjh0@users.noreply.github.com>
2024-03-20 22:31:51 +0900

    10.3 Serializer

--- categories/views.py
+++ categories/views.py
@@ -1,18 +1,18 @@
-from django.http import JsonResponse
-from django.core import serializers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .models import Category
+from .serializers import CategorySerializer
 
 # Create your views here.
 
 
+@api_view()
 def categories(request):
     all_categories = Category.objects.all()
-    return JsonResponse(
+    serializer = CategorySerializer(all_categories, many=True)
+    return Response(
         {
             "ok": True,
-            "categories": serializers.serialize(
-                "json",
-                all_categories,
-            ),
+            "categories": serializer.data,
         },
     )
