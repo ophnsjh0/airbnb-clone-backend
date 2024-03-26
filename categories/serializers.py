f00bc67cf986d3d9f29baa5a829d65bb2716f31a
ophnsjh0 <61132132+ophnsjh0@users.noreply.github.com>
2024-03-26 22:31:44 +0900

    10.12 ModelViewSet

--- categories/serializers.py
+++ categories/serializers.py
@@ -7,22 +7,3 @@ class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = "__all__"
-
-    # pk = serializers.IntegerField(read_only=True)
-    # name = serializers.CharField(
-    #     required=True,
-    #     max_length=50,
-    # )
-    # kind = serializers.ChoiceField(
-    #     choices=Category.CategoryKindChoices.choices,
-    # )
-    # created_at = serializers.DateTimeField(read_only=True)
-
-    # def create(self, validated_data):
-    #     return Category.objects.create(**validated_data)
-
-    # def update(self, instance, validated_data):
-    #     instance.name = validated_data.get("name", instance.name)
-    #     instance.kind = validated_data.get("kind", instance.kind)
-    #     instance.save()
-    #     return instance
