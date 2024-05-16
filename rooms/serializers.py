87ccb2664e062db940d066817989ec62c6d361a1
ophnsjh0 <61132132+ophnsjh0@users.noreply.github.com>
2024-05-16 23:48:34 +0900

    11.12 Serializer Context

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -1,10 +1,10 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from .models import Amenity, Room
 from users.serializers import TinyUserSerializer
 from categories.serializers import CategorySerializer
 
 
-class AmenitiesSerializer(ModelSerializer):
+class AmenitiesSerializer(serializers.ModelSerializer):
     class Meta:
         model = Amenity
         fields = (
@@ -13,17 +13,22 @@ class AmenitiesSerializer(ModelSerializer):
         )
 
 
-class RoomDetailSerializer(ModelSerializer):
+class RoomDetailSerializer(serializers.ModelSerializer):
     owner = TinyUserSerializer(read_only=True)
     amenities = AmenitiesSerializer(read_only=True, many=True)
     category = CategorySerializer(read_only=True)
+
+    rating = serializers.SerializerMethodField()
 
     class Meta:
         model = Room
         fields = "__all__"
 
+    def get_rating(self, room):
+        return room.rating()
 
-class RoomListSerializer(ModelSerializer):
+
+class RoomListSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
         fields = (
